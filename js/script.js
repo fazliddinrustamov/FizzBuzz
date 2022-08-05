0838bf804ba4f61ac5b4b18cb40032900526ee85
fazliddinrustamov <azizyuldashev19800@gmail.com>
2022-08-05 15:25:36 +0500

    add vars

--- js/script.js
+++ js/script.js
@@ -7,14 +7,17 @@ elSiteForm.addEventListener("submit", function(evt) {
   evt.preventDefault()
 
   let elNumTakerValue = elNumTaker.value;
+  let fizzNum = "3";
+  let buzzNum = "5";
+  let zeroNum = "0";
 
-  if (elNumTakerValue <= 0){
+  if (elNumTakerValue <= zeroNum){
     elResultTaker.textContent = "Write with positive number!"
-  } else if (elNumTakerValue % 3 == 0 && elNumTakerValue % 5 == 0) {
+  } else if (elNumTakerValue % fizzNum == zeroNum && elNumTakerValue % buzzNum == zeroNum) {
     elResultTaker.textContent = "FizzBuzz!"
-  } else if (elNumTakerValue % 3 == 0) {
+  } else if (elNumTakerValue % fizzNum == zeroNum) {
     elResultTaker.textContent = "Fizz!"
-  } else if (elNumTakerValue % 5 == 0) {
+  } else if (elNumTakerValue % buzzNum == zeroNum) {
     elResultTaker.textContent = "Buzz!"
   }
 })
